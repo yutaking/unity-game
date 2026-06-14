1ee4ee66d411fa7d257a63743f78d9aebf174ff8
kinrei <yuta1012.suzuki@gmail.com>
2026-06-14 10:12:59 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
